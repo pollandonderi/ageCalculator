c8cd019de765c2501fa0c150c44b59e99285f241
polland <pollandsabisa@gmail.com>
2023-05-03 00:55:21 +0300

    Done ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalculatorComponent } from './calculator/calculator.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component:CalculatorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
